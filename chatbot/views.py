3b77759f82fc71bffebf5911604566b392aff609
Thanh-Nguyen <nguyetha0725@gmail.com>
2024-11-27 00:33:10 -0600

    Adjust backend method for testing purposes

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,6 +1,10 @@
+import json
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .nlp_models.chatbot import Chatbot
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 # Create your views here.
 chatbot = Chatbot()
@@ -8,9 +12,10 @@ chatbot = Chatbot()
 @csrf_exempt
 def generate_response(request):
     if request.method == 'POST':
-        message = request.POST.get('message', '')
-        source_lang = request.POST.get('source_lang')
-        target_lang = request.POST.get('target_lang')
-        response = chatbot.process_input(message, source_lang, target_lang)
-        return JsonResponse({'message': response})
+        data = json.loads(request.body)
+        message = data.get('message')
+        if not message:
+            return JsonResponse({'message': 'Invalid message'})
+        response = chatbot.process_input(message)
+        return JsonResponse({'content': response, 'role': 'bot'})
     return HttpResponse('Invalid request method')
