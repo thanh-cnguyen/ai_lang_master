917bcab10c8b52ce253197721d96ea2abe494cdb
Tejas <tejas.pinjala@gmail.com>
2024-11-29 22:20:00 -0600

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import ChatContainer from './components/ChatContainer'
-import './styles/base.scss'
-import './utils/fetchInterceptor'
+// frontend/src/App.js
+import React, { useState } from 'react';
+import ChatBubble from './components/ChatBubble';
+import ChatContainer from './components/ChatContainer';
+import './styles/base.scss';
+import './styles/ChatBubble.scss';
+import './utils/fetchInterceptor';
 
 function App() {
+  const [chatStarted, setChatStarted] = useState(false);
+
+  const handleChatStart = () => {
+    setChatStarted(true);
+  };
+
   return (
     <div className="App">
-      <ChatContainer />
+      {chatStarted ? <ChatContainer /> : <ChatBubble onChatStart={handleChatStart} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
